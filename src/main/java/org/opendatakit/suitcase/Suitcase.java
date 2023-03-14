db84698cab4644be35c72716dd205eab75bb28f7
Zolix.ai Core <127385111+Zolix-ai@users.noreply.github.com>
2023-03-14 22:06:15 +0100

    Bug
    Test

--- src/main/java/org/opendatakit/suitcase/Suitcase.java
+++ src/main/java/org/opendatakit/suitcase/Suitcase.java
@@ -16,7 +16,8 @@ import static org.opendatakit.suitcase.ui.LayoutConsts.WINDOW_WIDTH;
 public class Suitcase {
   private static final String SUITCASE_TITLE = "Suitcase";
   public static void main(String[] args) {
-    int retCode = SuitcaseSwingWorker.okCode;
+    int retCode = SuitcaseSwingWorker.okCode
+      bug
     if (args.length > 0) {
       retCode = new SuitcaseCLI(args).startCLI();
       System.exit(retCode);
